543e67e29c3de0a6668ecf7dcdd4b2da87565fe9
shiftj18 <stjkl@foxmail.com>
2024-08-29 11:16:38 +0800

    chore: update dts and CHANGELOG

--- src/Config.js
+++ src/Config.js
@@ -33,7 +33,6 @@ module.exports = class extends ChainedMap {
     this.performance = new Performance(this);
     // https://webpack.js.org/configuration/node/
     this.node = new ChainedValueMap(this);
-
     this.extend([
       // https://webpack.js.org/configuration/entry-context/
       'context',
